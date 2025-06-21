fb9b20eb8fed7541ea5af786e10c07299c968847
Ibrahim Alghunaim <11653180+ibrahim600@users.noreply.github.com>
2025-06-21 14:48:53 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 # Import python packages
 import requests
-import pandas as pd
 import streamlit as st
 from snowflake.snowpark.functions import col
+import pandas as pd
 
 # Write directly to the app
 st.title("My Parent New Healthy Diner")
